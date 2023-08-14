c411f1e676b2912b0ae4baa4f78257df4bc55ed7
Rivansh Srivastava <48222792+RSbleedblue@users.noreply.github.com>
2023-08-14 20:03:53 +0530

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,1 +1,21 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void Bubblesort(int arr[]){
+        for(int i=0;i< arr.length-1;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]>arr[j]){
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+    }
+    public static void main(String args[]){
+        int arr[] = {22,3,1,4,5};
+        Bubblesort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
 
